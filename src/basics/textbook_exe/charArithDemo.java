d25c91600dfaabfa4f1d7dffbde986590c4fa3c2
Siva Naga Raju <tsnraju.official@gmail.com>
2023-08-12 12:57:19 +0530

    Text Example-3

---
+++ src/basics/textbook_exe/charArithDemo.java
@@ -0,0 +1,17 @@
+package basics.textbook_exe;
+
+public class charArithDemo {
+    public static void main(String args[]) {
+        char ch = 'X';
+        
+        System.out.println("ch = "+ch);
+
+        ch++;
+
+        System.out.println("ch++ = "+ch);
+
+        ch = 90;
+
+        System.out.println("ch now = "+ch);
+    }
+}
