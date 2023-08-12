8593e02c5ec4e152d30d72178a90509b4a46263d
Siva Naga Raju <tsnraju.official@gmail.com>
2023-08-12 12:46:25 +0530

    Text Example-2

--- src/basics/textbook_exe/hypotenuos.java
+++ src/basics/textbook_exe/hypotenuos.java
@@ -2,6 +2,10 @@ package basics.textbook_exe;
 
 public class hypotenuos {
     public static void main(String args[]) {
+        double x = 23.5;
+        double y = 35.8;
+        double z = Math.sqrt((x*x) + (y*y));
         
+        System.out.println("Hypotenuos = "+z);
     }
 }
