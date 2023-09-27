0be9287d4c2aadb20d1889ac403c371312587eae
Siva Naga Raju <tsnraju.official@gmail.com>
2023-09-27 10:34:19 +0530

    VarArgs modified

--- src/classes_and_methods/varargsDemo.java
+++ src/classes_and_methods/varargsDemo.java
@@ -11,9 +11,24 @@ public class varargsDemo {
         System.out.println();
     }
 
+    static void vaTest2(String x, boolean y, int ...z){
+        System.out.println("Number of VarArgs in vaTest2: "+z.length);
+        System.out.println("Contents: ");
+        System.out.println("    The String: "+x);
+        System.out.println("    Boolean: "+y);
+
+        for(int i=0; i<z.length; i++){
+            System.out.println("    args "+i+": "+z[i]);
+        }
+        System.out.println();
+    }
+
     public static void main(String args[]){
         vaTest(10);
         vaTest(10,30,40,50,60,70);
         vaTest();
+
+        vaTest2("Siva Naga Raju", false, 10,11,12,13,14);
+        vaTest2("Tirumalasetti", true, 100);
     }
 }
