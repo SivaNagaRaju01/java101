afb3e6fbc9a765d51a282112789f914889568ba2
Siva Naga Raju <tsnraju.official@gmail.com>
2023-09-27 10:40:37 +0530

    Overloading VarArgs Method

--- src/classes_and_methods/varargsDemo.java
+++ src/classes_and_methods/varargsDemo.java
@@ -2,11 +2,32 @@ package classes_and_methods;
 
 public class varargsDemo {
     static void vaTest(int ...x){
-        System.out.println("Number of VarArgs: "+x.length);
+        System.out.println("Number of Int VarArgs: "+x.length);
         System.out.println("Contents: ");
 
         for(int i=0; i<x.length; i++){
             System.out.println("    arg "+i+": "+x[i]);
+        }
+        System.out.println();
+    }
+
+    static void vaTest(boolean ...x){
+        System.out.println("Number of Boolean VarArgs: "+x.length);
+        System.out.println("Contents: ");
+
+        for(int i=0; i<x.length; i++){
+            System.out.println("    arg "+i+": "+x[i]);
+        }
+        System.out.println();
+    }
+
+    static void vaTest(String x, int ...y){
+        System.out.println("Number of Hybrid VarArgs: "+y.length);
+        System.out.println("Contents: ");
+        System.out.println("    The String: "+x);
+
+        for(int i=0; i<y.length; i++){
+            System.out.println("    arg "+i+": "+y[i]);
         }
         System.out.println();
     }
@@ -26,7 +47,8 @@ public class varargsDemo {
     public static void main(String args[]){
         vaTest(10);
         vaTest(10,30,40,50,60,70);
-        vaTest();
+        vaTest("Siva Naga Raju",10,20,30);
+        vaTest(true,false,false);
 
         vaTest2("Siva Naga Raju", false, 10,11,12,13,14);
         vaTest2("Tirumalasetti", true, 100);
