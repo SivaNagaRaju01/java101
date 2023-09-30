fe7dd2ec1106073a111c867e4b262e8432c06684
Siva Naga Raju <tsnraju.official@gmail.com>
2023-09-30 15:29:36 +0530

    Modifying an Interface Implimentation

--- src/Interfaces/Series.java
+++ src/Interfaces/Series.java
@@ -9,23 +9,32 @@ public interface Series {
 class ByTwos implements Series {
     int start;
     int val;
+    int prev;
 
     ByTwos() {
         start = 0;
         val = 0;
+        prev = -2;
     }
 
     public int getNext() {
+        prev = val;
         val += 2;
         return val;
     }
 
     public void reset() {
         val = start;
+        prev = start - 2;
     }
 
     public void setStart(int x) {
         start = x;
         val = x;
+        prev = x - 2;
+    }
+
+    int getPrevious() {
+        return prev;
     }
 }
