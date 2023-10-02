9bb86a619349720e4a76d50186c878b15a469113
Siva Naga Raju <tsnraju.official@gmail.com>
2023-10-02 11:25:41 +0530

    Added Read class to basics package

---
+++ src/basics/Read.java
@@ -0,0 +1,16 @@
+package basics;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Read {
+    public int read_int()throws Exception {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(ir);
+        int x;
+
+        x = Integer.parseInt(br.readLine());
+
+        return x;
+    }
+}
