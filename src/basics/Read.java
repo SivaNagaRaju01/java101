01c45ce89aebdfdba170f6e32901f98440dbbb32
Siva Naga Raju <tsnraju.official@gmail.com>
2023-10-31 09:33:01 +0530

    A simple Change

--- src/basics/Read.java
+++ src/basics/Read.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 
 public class Read {
     public int read_int()throws Exception {
+        System.out.println("You need to input an Integer Value...");
         InputStreamReader ir = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(ir);
         int x;
