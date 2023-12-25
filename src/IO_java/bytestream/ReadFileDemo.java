12e4aafd3dbff38efa7d6c293efe7c8ebc177be1
SivaNagaRaju01 <tsnraju.official@gmail.com>
2023-12-25 10:08:13 +0530

    File Reader v2

--- src/IO_java/bytestream/ReadFileDemo.java
+++ src/IO_java/bytestream/ReadFileDemo.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class ReadFileDemo {
     public static void main(String args[]) {
-        FileInputStream fn;
+        FileInputStream fn = null;
 
         if(args.length != 1) {
             System.out.println("Please specify file name at the terminal.");
@@ -13,25 +13,22 @@ public class ReadFileDemo {
 
         try {
             fn = new FileInputStream(args[0]);
-        }catch (FileNotFoundException fnf) {
-            System.out.println("This file Does not Exist.");
-            return;
-        }
-
-        try {
             int i = 0;
             while(i != -1) {
                 i = fn.read();
                 if(i!=-1) System.out.print((char)i);
             }
+        }catch (FileNotFoundException fnf) {
+            System.out.println("This file Does not Exist.");
+            return;
         }catch(IOException ioe) {
             System.out.println("\nAn Error Occured while reading.");
-        }
-
-        try {
-            fn.close();
-        }catch(IOException ioe) {
-            System.out.println("An Error Occured while closing file.");
+        }finally {
+            try {
+            if(fn != null) fn.close();
+            }catch(IOException ioe) {
+                System.out.println("An Error Occured while closing file.");
+            }
         }
     }
 }
